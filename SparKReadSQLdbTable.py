5d22709e03db8df677fd4e0bbc32ab1a666cf5d2
Momataj Begum <40410209+momataj@users.noreply.github.com>
2021-11-23 16:00:05 +1300

    Add files via upload
    Read a table from SQL database using pyspark in the databricks

---
+++ SparKReadSQLdbTable.py
@@ -0,0 +1,38 @@
+
+# Read Data from SQL Database Table using Pyspark
+
+def read_db_data(SQL_Query,DatabaseName):
+  # Create function of read db table and create dataframe 
+  Credentials=sqlvm_jdbc_connection(DatabaseName=DatabaseName)
+  try: 
+    # Read Query
+    queryDF = spark.read.format("jdbc")\
+        .option("driver", Credentials['driver'])\
+        .option("url", Credentials['url'])\
+        .option("fetchsize",50000)\
+        .option("query", SQL_Query) \
+        .load()
+  except Exception as e:
+    print(e.args)
+  return queryDF
+
+
+def sql_query(TableName):
+  sql_query="""
+      Select * from """+TableName+"""
+      """
+  return sql_query
+
+def main():
+
+  DatabaseName='ABC' #name of the database
+  SQL_Query=sql_query(TableName)
+  queryDF=read_db_data(SQL_Query,DatabaseName)
+
+  # create a temp view in spark
+  queryDF.createOrReplaceTempView("queryDF")
+  print(queryDF.count())
+  queryDF.display()
+
+if __name__ == "__main__":
+    main()
