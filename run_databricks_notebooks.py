63d6b5c1713bb444506ed2b32a6a2a3e1a3bfee4
Momataj Begum <40410209+momataj@users.noreply.github.com>
2021-12-28 10:47:08 +1300

    Update run_databricks_notebooks.py

--- run_databricks_notebooks.py
+++ run_databricks_notebooks.py
@@ -6,7 +6,9 @@ ExceptionsArray = []  # declare an excetionArrary to check error in case noteboo
 #%run ./hello_world   Test notebook run with this command 
 
 try:
-  dbutils.notebook.run("./hello_world", 600, {"parameter": "word_count", "batch_id": BatchGuid})  # Parameters from notebook 
+  dbutils.notebook.run("./hello_world", 600, {"parameter": "country", "batch_id": BatchGuid})  # Parameters from notebook 
+  dbutils.notebook.run("./word_count", 600, {})  # Without parameters 
+  
 
 except Exception as e:
   print(e)
