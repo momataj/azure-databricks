22363e70c48954d789db8305d535dbf0c5e788b3
Momataj Begum <40410209+momataj@users.noreply.github.com>
2021-12-28 10:53:56 +1300

    Update run_databricks_notebooks.py

--- run_databricks_notebooks.py
+++ run_databricks_notebooks.py
@@ -1,3 +1,14 @@
+"""
+Run Multiple databricks notebooks at a time from one Notebook using python
+
+Example: Run end to end solution for data pipleines 
+- Data ingestion application Notebook
+- Transformation application Notebook
+- Refine application Notebook
+
+Call all above applications in run_databricks_notebooks and schedule them using apache airflow/Azure data factory
+"""
+
 import uuid
 BatchGuid = str(uuid.uuid4()) 
 
@@ -6,8 +17,9 @@ ExceptionsArray = []  # declare an excetionArrary to check error in case noteboo
 #%run ./hello_world   Test notebook run with this command 
 
 try:
-  dbutils.notebook.run("./hello_world", 600, {"parameter": "country", "batch_id": BatchGuid})  # Parameters from notebook 
-  dbutils.notebook.run("./word_count", 600, {})  # Without parameters 
+  dbutils.notebook.run("./Data_ingestion", 600, {"parameter": "country", "batch_id": BatchGuid})  # Parameters from notebook 
+  dbutils.notebook.run("./Transformation", 600, {"parameter": "country", "batch_id": BatchGuid}) 
+  dbutils.notebook.run("./Refine", 600, {})  # Without parameters  
   
 
 except Exception as e:
