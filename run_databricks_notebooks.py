d19a4892e04ae781bbadda4db6785b4dd14e07e1
Momataj Begum <40410209+momataj@users.noreply.github.com>
2021-12-28 10:43:58 +1300

    Create run_databricks_notebooks.py

---
+++ run_databricks_notebooks.py
@@ -0,0 +1,19 @@
+import uuid
+BatchGuid = str(uuid.uuid4()) 
+
+ExceptionsArray = []  # declare an excetionArrary to check error in case notebooks failure. 
+
+#%run ./hello_world   Test with notebook with this command 
+
+try:
+  dbutils.notebook.run("./hello_world", 600, {"parameter": "word_count", "batch_id": BatchGuid})  # Parameters from notebook 
+
+except Exception as e:
+  print(e)
+  ExceptionsArray.append(e)
+
+
+if len(ExceptionsArray)  > 0:
+  raise ExceptionsArray[0]
+  
+dbutils.notebook.exit('Complete')  
